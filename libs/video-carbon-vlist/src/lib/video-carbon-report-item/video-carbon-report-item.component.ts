508153837fb97e2f5c1b95b8c0a4e7e0306a42e3
pingyuan <ping.yuan@finastra.com>
2024-03-19 09:18:30 +0100

    fine tunning tree component

--- libs/video-carbon-vlist/src/lib/video-carbon-report-item/video-carbon-report-item.component.ts
+++ libs/video-carbon-vlist/src/lib/video-carbon-report-item/video-carbon-report-item.component.ts
@@ -1,19 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, OnInit, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoItem } from '@video-carbon-reduce-report/shared';
 import { FileSizePipe } from '../video-carbon-card/video-size.pipe';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-video-carbon-report-item',
   standalone: true,
-  imports: [CommonModule, FileSizePipe],
+  imports: [
+    CommonModule,
+    FileSizePipe,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './video-carbon-report-item.component.html',
   styleUrl: './video-carbon-report-item.component.scss',
 })
-export class VideoCarbonReportItemComponent {
+export class VideoCarbonReportItemComponent implements OnInit {
   videos = input<VideoItem[]>();
   impression = input<number>();
-
+  impressionValue = signal<number>(0);
+  impressionFormControl = new FormControl();
   totalSize(size: number, impression: number | undefined): number {
     if (impression) {
       return size * impression;
@@ -21,7 +30,11 @@ export class VideoCarbonReportItemComponent {
       return 0;
     }
   }
-
+  ngOnInit(): void {
+    this.impressionFormControl.valueChanges.subscribe((data) => {
+      this.impressionValue.set(data);
+    });
+  }
   emission(size: number): number {
     const mb = size / 1024;
 
